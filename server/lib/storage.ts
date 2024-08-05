87a42a67c27f47ea164770c61a45e812cdf70940
viandwi24 <viandwi24@pm.me>
2024-08-05 10:30:05 +0700

    fix: add storage env

--- server/lib/storage.ts
+++ server/lib/storage.ts
@@ -1,9 +1,12 @@
+import path from 'path'
 import { createStorage } from "unstorage"
 import fsDriver from "unstorage/drivers/fs-lite"
 
+export const storagepath = path.join(process.cwd(), `${process.env.STORAGE_PATH || './storages/uploads'}`)
 const storage = createStorage({
   driver: fsDriver({
-    base: './storages/uploads'
+    // base: process.env.STORAGE_PATH || './storages/uploads'
+    base: storagepath
   })
 })
 
--- server/plugins/db.ts
+++ server/plugins/db.ts
@@ -1,8 +1,13 @@
+import path from 'path'
 import { prisma } from "@/server/lib/db"
+import { storage } from '@/server/lib/storage'
 
 export default defineNitroPlugin(async (nitroApp) => {
   console.log(`[nitro:db] connecting to database`)
   await prisma.$connect()
   console.log(`[nitro:db] connected to database`)
   console.log(`[nitro:db] ${await prisma.flaff.count()} flaffs found, ${await prisma.file.count()} files found`)
+
+  const storagepath = path.join(process.cwd(), `${process.env.STORAGE_PATH || './storages/uploads'}`)
+  console.log(`[nitro:storage] load storages in path: ${storagepath}`)
 })
